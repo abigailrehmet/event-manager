00611fb4d2d012e8a06d9622c118050569edec65
Abby Work <arehmet@Abigails-MacBook-Pro.local>
2021-06-08 16:08:34 -0400

    error checking in helpers

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -5,6 +5,8 @@ import EventList from './EventList';
 import PropTypes from 'prop-types';
 import PropsRoute from './PropsRoute';
 import Event from './Event';
+import { Switch } from 'react-router-dom';
+import EventForm from './EventForm';
 
 class Editor extends React.Component {
   constructor(props) {
@@ -34,12 +36,15 @@ class Editor extends React.Component {
 
     return (
       <div>
-      <Header />
-      <div className="grid">
-        <EventList events={events} />
-        <PropsRoute path="/events/:id" component={Event} event={event} />
+        <Header />
+        <div className="grid">
+          <EventList events={events} activeId={Number(eventId)} />
+          <Switch>
+            <PropsRoute path="/events/new" component={EventForm} />
+            <PropsRoute path="/events/:id" component={Event} event={event} />
+          </Switch>
+        </div>
       </div>
-    </div>
     );
   }
 }
--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -4,37 +4,40 @@ import { Link } from 'react-router-dom';
 
 class EventList extends React.Component {
   renderEvents() {
-    const { events } = this.props;
-    events.sort(
-      (a, b) => new Date(b.event_date) - new Date(a.event_date),
-    );
+    const { activeId, events } = this.props;
+    events.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
 
     return events.map(event => (
       <li key={event.id}>
-      <Link to={`/events/${event.id}`}>
-        {event.event_date}
-        {' - '}
-        {event.event_type}
-      </Link>
-    </li>
+        <Link to={`/events/${event.id}`} className={activeId === event.id ? 'active' : ''}>
+          {event.event_date}
+          {' - '}
+          {event.event_type}
+        </Link>
+      </li>
     ));
   }
 
   render() {
     return (
       <section className="eventList">
-        <h2>Events</h2>
-        <ul>{this.renderEvents()}</ul>
-      </section>
+      <h2>
+        Events
+        <Link to="/events/new">New Event</Link>
+      </h2>
+      <ul>{this.renderEvents()}</ul>
+    </section>
     );
   }
 }
 
 EventList.propTypes = {
+  activeId: PropTypes.number,
   events: PropTypes.arrayOf(PropTypes.object),
 };
 
 EventList.defaultProps = {
+  activeId: undefined,
   events: [],
 };
 
