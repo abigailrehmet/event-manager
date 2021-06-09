b028ada36f2265a6aaba67a5c00755d5ce430747
Abby Work <arehmet@Abigails-MacBook-Pro.local>
2021-06-09 12:57:00 -0400

    handle non existent events

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { isEmptyObject, validateEvent } from '../helpers/helpers';
 import { Link } from 'react-router-dom';
+import EventNotFound from './EventNotFound';
 
 
 class EventForm extends React.Component {
@@ -63,7 +64,11 @@ class EventForm extends React.Component {
   }
 
   render() {
-    const {event} = this.state;
+    const { event } = this.state;
+    const { path } = this.props;
+
+    if (!event.id && path === '/events/:id/edit') return <EventNotFound />;
+
     const cancelURL = event.id ? `/events/${event.id}` : '/events';
     const title = event.id ? `${event.event_date} - ${event.event_type}` : 'New Event';
 
@@ -162,6 +167,7 @@ class EventForm extends React.Component {
 EventForm.propTypes = {
   event: PropTypes.shape(),
   onSubmit: PropTypes.func.isRequired,
+  path: PropTypes.string.isRequired,
 };
 
 EventForm.defaultProps = {
